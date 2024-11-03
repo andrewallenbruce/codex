426e38b5f3027bc3c8181f5e34a291e99c536a90
Andrew Bruce <andrewbruce.himni@gmail.com>
2024-11-02 22:20:14 -0700

    export helpers

--- R/helpers.R
+++ R/helpers.R
@@ -1,20 +1,88 @@
-#' @noRd
+#' Unique Lengths
+#' @param x vector
+#' @returns vector
+#' @autoglobal
+#' @keywords internal
+#' @export
 unique_vlength <- \(x) collapse::funique(collapse::vlengths(x, use.names = FALSE))
 
-#' @noRd
+#' Unique Values with NAs Removed
+#' @param x vector
+#' @returns vector
+#' @autoglobal
+#' @keywords internal
+#' @export
 unique_narm <- \(x) collapse::funique(collapse::na_rm(x))
 
-#' @noRd
+#' Max of Lengths
+#' @param x vector
+#' @returns integer
+#' @autoglobal
+#' @keywords internal
+#' @export
 max_vlength <- \(x) collapse::fmax(collapse::vlengths(x, use.names = FALSE))
 
-#' @noRd
+#' Is Empty
+#' @param x vector
+#' @returns logical
+#' @autoglobal
+#' @keywords internal
+#' @export
 empty <- \(x) vctrs::vec_is_empty(x)
 
-#' @noRd
+#' Chop Vector by Group
+#' @param v vector
+#' @param g group
+#' @returns vector
+#' @autoglobal
+#' @keywords internal
+#' @export
 chop <- \(v, g) vctrs::vec_chop(v, sizes = vctrs::vec_run_sizes(g))
 
-#' @noRd
+#' Subset Vector at Index
+#' @param x vector
+#' @param i index
+#' @returns vector
+#' @autoglobal
+#' @keywords internal
+#' @export
 take_at <- \(x, i = 1) stringfish::sf_substr(x, start = i, stop = i, nthreads = 4L)
 
-#' @noRd
-detect_az <- \(x) x[stringfish::sf_grepl(x, "[A-Z]{1}", nthreads = 4L)]
+#' Subset Vector by Range
+#' @param x vector
+#' @param i index start
+#' @param z index end
+#' @returns vector
+#' @autoglobal
+#' @keywords internal
+#' @export
+sf_sub <- \(x, i = 1, z) stringfish::sf_substr(x, start = i, stop = z, nthreads = 4L)
+
+#' Extract by Regex
+#' @param s subject
+#' @param p pattern
+#' @returns vector
+#' @autoglobal
+#' @keywords internal
+#' @export
+sf_extract <- \(s, p) x[stringfish::sf_grepl(s, p, nthreads = 4L)]
+
+#' Sort Order
+#' @param x vector
+#' @returns vector
+#' @autoglobal
+#' @keywords internal
+#' @export
+sort_order <- \(x) {
+
+  sort <- stringr::str_sort(x, numeric = TRUE)
+  alph <- stringr::str_extract_all(sort, stringr::regex("[A-Z]"))
+  numb <- stringr::str_extract_all(sort, stringr::regex("[0-9]"))
+  alph <- purrr::list_c(alph)
+  numb <- purrr::list_c(numb)
+
+  paste0(
+    paste0(alph, collapse = ""),
+    paste0(numb, collapse = "")
+  )
+}
--- R/random_hcpcs.R
+++ R/random_hcpcs.R
@@ -17,36 +17,9 @@ random_hcpcs <- function(n = 10) {
 
   c(
     sample(c(ltrs, 0:9), size = sample.int(5, 1)),
-    stringfish::sf_substr(sample(h, n), 1, 2, nthreads = 4L),
-    stringfish::sf_substr(sample(h, n), 1, 3, nthreads = 4L),
-    stringfish::sf_substr(sample(h, n), 1, 4, nthreads = 4L),
-    stringfish::sf_substr(sample(h, n), 1, 5, nthreads = 4L)
+    sfsub(sample(h, n), z = 2),
+    sfsub(sample(h, n), z = 3),
+    sfsub(sample(h, n), z = 4),
+    sfsub(sample(h, n), z = 5)
   )
-}
-
-#' Generate Random HCPCS Codes
-#'
-#' @param n <integer> number of codes
-#'
-#' @param l <integer> length of codes
-#'
-#' @returns <character> vector
-#'
-#' @examples
-#' random_hcpcs2(n = 5, l = 5)
-#'
-#' @autoglobal
-#'
-#' @noRd
-random_hcpcs2 <- function(n, l) {
-
-  h <- stringfish::convert_to_sf(
-    get_pin("hcpcs_vec")
-  )
-
-  stringfish::sf_substr(
-    sample(h, size = n),
-    start = 1,
-    stop = l,
-    nthreads = 4L)
 }
